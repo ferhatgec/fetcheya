801c9009e37df9a94a082be4abadf39de1b03e8e
ferhatgec <ferhatgectao@gmail.com>
2020-07-28 14:08:51 +0300

    Added Slackware support and Logos<dot>cpp

--- make.sh
+++ make.sh
@@ -1,8 +1,3 @@
-#   MIT License
-#   Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
-#   Distributed under the terms of the MIT License.
-# 
-
 #!/bin/sh
 
-g++ -I./include/ ./src/Fetcheya.cpp -o fetcheya && ./fetcheya
+g++ -I./include/ ./src/Logos.cpp ./src/Fetcheya.cpp -o fetcheya && ./fetcheya
